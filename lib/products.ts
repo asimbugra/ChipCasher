2985c1c5190d2427d9212b39ed05a015dda50cb5
asimbugra <asimbugra1@gmail.com>
2023-10-14 01:50:14 +0300

    update

---
+++ lib/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    id: 'box-of-cookies',
+    name: 'Sandwich',
+    description: 'A sandwich prepared with fresh ingredients',
+    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.05,
+    priceUsd: 2,
+  },
+  {
+    id: 'basket-of-cookies',
+    name: 'Cookies',
+    description: 'A large basket of handmade cookies',
+    unitName: 'basket',
+    priceSol: 0.1,
+    priceUsd: 3,
+  },
+  {
+    id: 'COKE',
+    name: 'Cola',
+    description: 'A refreshing carbonated beverage',
+    unitName: 'basket',
+    priceSol: 0.1,
+    priceUsd: 1,
+  }]
